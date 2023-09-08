a8fe7834d2a00f5dce74a58a37c49488f1c0c217
Anas AitZouinet <anasaw236@gmail.com>
2023-09-08 01:12:09 +0100

    fix 4

--- app/(api)/trending/route.tsx
+++ app/(api)/trending/route.tsx
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const type = searchParams.get("type");
-  const res = await fetch("http://localhost:4000/anime/home", {
+  const res = await fetch(`${process.env.NEXT_API_ANIW}/anime/home`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
